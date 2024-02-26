33936a70662f4b3cbaf99e2f2097df2a04959423
Dhanush Bharath Raj <46651355+DhanushAki@users.noreply.github.com>
2024-02-26 01:42:20 +0000

    Added some Projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,9 @@
 import pro1 from "../assets/TourMate.jpg";
 import pro2 from "../assets/memoryTester.png";
 import pro3 from "../assets/drumKit.png";
+import pro4 from "../assets/car.png";
+import pro5 from "../assets/image_compress.png";
+import pro6 from "../assets/info_viz.png";
 
 
 const ProjectCardData = [
@@ -21,10 +24,31 @@ const ProjectCardData = [
     {
         imgsrc: pro3,
         title: "Drum Kit",
-        text: "Unleash your rhythmic creativity with this interactive drum kit, Eexplore the beats at your fingertips.",
+        text: "Unleash your Rhythmic creativity with this interactive drum kit, Eexplore the beats at your fingertips.",
         view: "https://dhanushaki.github.io/Drum_Kit/",
         source: "https://github.com/DhanushAki/Drum_Kit",
 
+    },
+    {
+        imgsrc: pro4,
+        title: "Steering Wheel Angle Predection",
+        text: "Utilizing Deep Learning and Neural Networks to predict the steering wheel angle",
+        view: "https://github.com/DhanushAki/Steering-wheel-prediction/blob/master/IRJET-V7I3924.pdf",
+        source: "https://github.com/DhanushAki/Steering-wheel-prediction/tree/master",
+    },
+    {
+        imgsrc: pro5,
+        title: "Image Compressing using K-Means",
+        text: "Compression of image with 16 million colors to 16 colors with the help of K-Means clustering",
+        view: "https://github.com/DhanushAki/Image_Compression_K-means/blob/master/Fully_Completed.ipynb",
+        source: "https://github.com/DhanushAki/Image_Compression_K-means/tree/master",
+    },
+    {
+        imgsrc: pro6,
+        title: "Meta Kaggle and Trends in Data Science and ML",
+        text: "Utilizing Meta Kaggle Code datasets to explore and gain insights on team performance",
+        view: "https://github.com/DhanushAki",
+        source: "https://github.com/DhanushAki",
     },
 ];
 
